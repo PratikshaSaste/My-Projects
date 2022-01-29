5080042322fcf1313e44eeb9f85d0180c47b3154
Pratiksha206 <pratiksha@profecialinks.com>
2022-01-29 16:32:33 +0530

    creating first react app

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App=()=>{
+  return(
+    <h1>Hello! My name is Pratiksha..</h1>
+  )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
