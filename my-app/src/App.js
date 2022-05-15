f2dfbe8cdc1d0f670de12a0c1772e17299ef1365
Pratiksha206 <pratiksha@profecialinks.com>
2022-05-15 14:00:03 +0530

    using stateful list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,42 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Chocolates',
-      amount: 94.12,
-      date: new Date(2022, 11, 4),
-    },
-    { id: 'e2', title: 'Mac Book', amount: 1000.49, date: new Date(2022, 8, 1) },
-    {
-      id: 'e3',
-      title: 'Teddy Bear',
-      amount: 194.67,
-      date: new Date(2022, 11, 30),
-    },
-    {
-      id: 'e4',
-      title: 'Books',
-      amount: 450,
-      date: new Date(2022, 8, 12),
-    },
-    {
-      id: 'e5',
-      title: 'Guitar',
-      amount: 550,
-      date: new Date(2022, 2, 9),
-    },
-    {
-      id: 'e6',
-      title: 'Nike shoes',
-      amount: 500,
-      date: new Date(2022, 9, 13),
-    },
-    {
-      id: 'e7',
-      title: 'Lether Jacket',
-      amount: 700,
-      date: new Date(2022, 1, 18),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses expenseitems={expenses}/>
     </div>
   );
