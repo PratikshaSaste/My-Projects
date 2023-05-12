85c860a8e552c8a5bdad7387c8836af7f9d9f382
Pratiksha206 <pratiksha@profecialinks.com>
2023-05-12 22:19:59 +0530

    updated

--- react-myportfolio-website/src/components/About/About.jsx
+++ react-myportfolio-website/src/components/About/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './About.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
-export const About = () => {
+ const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+       <div className="container about__container">
+         <div className="about__me">
+           <div className="about__me-image">
+             <img src={ME} alt="About Image" />
+           </div>
+         </div>
+         <div className="about__content">
+           <div className="about__cards">
+            <article className="about__card">
+             <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>2+ Years</small>
+            </article>
+
+            <article className="about__card">
+             <FiUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>100+ Clients</small>
+            </article>
+
+            <article className="about__card">
+             <VscFolderLibrary className="about__icon"/>
+              <h5>Projects</h5>
+              <small>10+ Completed projects</small>
+            </article>
+            </div>
+
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Excepturi deserunt facere ex iusto veritatis? 
+            Nihil dolor porro modi architecto commodi ex nulla quos, provident vel eum ea excepturi itaque necessitatibus?
+           </p>
+
+           <a href="#contact" className="btn btn-primary">Let's Talk</a>
+         </div>        
+       </div>
+    </section>
   )
 }
 
